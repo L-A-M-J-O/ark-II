0d064ff75e78c3495bb831dd1333fc22d39ad162
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-02-11 17:16:21 +0100

    twoth-commit
    Organizacion profecional de carpetas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 
+import { Routes, Route,} from 'react-router-dom'
+import { Home } from './Home';
+import { Dino } from './Dino';
+
+// Logica se va a mostrar Ark
+// DLC cada DLC mostrara cada dino que contiene ese map y solo ese map
+// Dino opcion de mirar como tamear y que mapa va estar 
+// home, dlc, news, servidores,  about team.
+
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Dino" element={<Dino/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/Dino.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Dino = () => {
+  return (
+    <div>
+      <h1>DINO</h1>
+    </div>
+  )
+}
