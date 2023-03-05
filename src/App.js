27224853498e45e5b9adaaba40bd594037a75e40
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-03-05 14:37:26 +0100

    1-commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ import './App.css';
 // Layaout
 import Layaout from './layout/Layaout';
 
-// Logica se va a mostrar Ark
-// DLC cada DLC mostrara cada dino que contiene ese map y solo ese map
-// Dino opcion de mirar como tamear y que mapa va estar 
-// home, dlc, news, servidores,  about team.
-
 function App() {
   return (
     <>
