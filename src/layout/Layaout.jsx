57f309565f06726b377d2d62d83526877052bc0f
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-03-09 09:41:20 +0100

    Update Layaout.jsx
    ghh

--- src/layout/Layaout.jsx
+++ src/layout/Layaout.jsx
@@ -5,13 +5,13 @@ import './Layaout.css';
 import { Routes, Route,} from 'react-router-dom';
 
 import Home from '../view/Home/Home';
-import SideBar from '../components/Navbar/SideBar';
+// import SideBar from '../components/Navbar/SideBar';
 
 function Layaout() {
   return (
     <section className='container-fluid'>
       <header>
-        <SideBar/>
+        {/* <SideBar/> */}
       </header>
               {/* <video className='video-main m-0 p-0' width='100%' height='100%' autoPlay loop muted playsInline>
                 <source src='../video/videoIsland.mp4' type='video/mp4'/>
